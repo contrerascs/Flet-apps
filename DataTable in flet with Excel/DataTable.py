da17eab8c9d094fc2bd4cb55125fb977f6114ac4
contrerascs <60404702+contrerascs@users.noreply.github.com>
2025-01-26 22:46:37 -0600

    DataTable with Excel

--- DataTable in flet with Excel/DataTable.py
+++ DataTable in flet with Excel/DataTable.py
@@ -1,1 +1,12 @@
 import flet as ft
+
+def main(page: ft.Page):
+    page.bgcolor = ft.colors.BLUE_GREY_800
+    page.title = 'DataTable in Flet with Excel'
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    titulo = ft.Text('DataTable in Flet', size=24, color=ft.colors.WHITE)
+
+    page.add(titulo)
+
+ft.app(target=main)

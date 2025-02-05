5a74a1d452943ef3e7245a2e78a98083c3f44df9
contrerascs <60404702+contrerascs@users.noreply.github.com>
2025-02-04 23:04:31 -0600

    Tabs in flet

--- Tabs with Flet/Tabs.py
+++ Tabs with Flet/Tabs.py
@@ -3,5 +3,11 @@ import random
 
 def main(page: ft.Page):
     page.bgcolor = ft.colors.BLUE_GREY_800
+    page.title = 'Tabs in Flet'
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    titulo = ft.Text('Ejemplo de Tabs in Flet', size=24, color=ft.colors.WHITE)
+
+    page.add(titulo)
 
 ft.app(target=main)

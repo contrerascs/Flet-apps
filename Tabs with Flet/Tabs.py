eaa9349eb3f9c6c35318b3b840832394e4da569e
contrerascs <60404702+contrerascs@users.noreply.github.com>
2025-02-07 22:52:40 -0600

    Tabs with flet

--- Tabs with Flet/Tabs.py
+++ Tabs with Flet/Tabs.py
@@ -8,11 +8,29 @@ def main(page: ft.Page):
 
     titulo = ft.Text('Ejemplo de Tabs in Flet', size=24, color=ft.colors.WHITE)
 
+    def generar_tareas():
+        tareas = ['Hacer la compra', 'Llamar al médico', 'Estudiar para el examen',
+                  'Hacer ejercicio', 'Leer un libro', 'Preparar la cena']
+        return [random.choice(tareas) for _ in range(5)]
+    
+    lista_tareas = ft.ListView(spacing=10, padding=20)
+
+    def actualizar_tareas():
+        lista_tareas.controls.clear()
+        for tarea in generar_tareas():
+            lista_tareas.controls.append(ft.Text, color = ft.colors.WHITE)
+
     tabs = ft.Tabs(
         selected_index=0,
-        
+        animation_duration=300,
+        tabs=[
+            ft.Tab(text='Tareas', icon=ft.icons.LIST_ALT),
+            ft.Tab(text='Perfil', icon=ft.icons.PERSON),
+            ft.Tab(text='Configuración', icon=ft.icons.SETTINGS)
+        ],
+        expand=1
     )
 
-    page.add(titulo)
+    page.add(titulo,tabs)
 
 ft.app(target=main)

7763c07ea3de4fa13bb951ec23cad2be6ea3e0f8
contrerascs <60404702+contrerascs@users.noreply.github.com>
2025-02-10 22:04:20 -0600

    Example Tabs with Flet

--- Tabs with Flet/Tabs.py
+++ Tabs with Flet/Tabs.py
@@ -11,22 +11,40 @@ def main(page: ft.Page):
     def generar_tareas():
         tareas = ['Hacer la compra', 'Llamar al médico', 'Estudiar para el examen',
                   'Hacer ejercicio', 'Leer un libro', 'Preparar la cena']
-        return [random.sample(tareas) for _ in range(5)]
+        return random.sample(tareas,4)
     
     lista_tareas = ft.ListView(spacing=10, padding=20)
 
     def actualizar_tareas():
         lista_tareas.controls.clear()
         for tarea in generar_tareas():
-            lista_tareas.controls.append(ft.Text, color = ft.colors.WHITE)
+            lista_tareas.controls.append(ft.Text(tarea, color = ft.colors.WHITE))
+        page.update()
+
+    actualizar_tareas()
+
+    btn_actualizar = ft.ElevatedButton('Actualizar Tareas', on_click=lambda _: actualizar_tareas())
+
+    contenido_tareas = ft.Column([lista_tareas, btn_actualizar])
+
+    #Contenido para pestaña de Perfil
+    campo_nombre = ft.TextField(label='Nombre', bgcolor=ft.colors.BLUE_GREY_700)
+    campo_email = ft.TextField(label='Email', bgcolor=ft.colors.BLUE_GREY_700)
+    btn_guadar = ft.ElevatedButton('Guardar Perfil')
+    contenido_perfil = ft.Column([campo_nombre, campo_email, btn_guadar])
+    
+    #Contenido para pestaña de Configuracion
+    switch_notificaciones = ft.Switch(label='Notificaciones', value=True)
+    slider_volumen = ft.Slider(min=0, max=100, divisions=10, label='Volumen')
+    contenido_config = ft.Column([switch_notificaciones, slider_volumen])
 
     tabs = ft.Tabs(
-        selected_index=0,
+        selected_index=0, 
         animation_duration=300,
         tabs=[
-            ft.Tab(text='Tareas', icon=ft.icons.LIST_ALT),
-            ft.Tab(text='Perfil', icon=ft.icons.PERSON),
-            ft.Tab(text='Configuración', icon=ft.icons.SETTINGS)
+            ft.Tab(text='Tareas', icon=ft.icons.LIST_ALT, content=contenido_tareas),
+            ft.Tab(text='Perfil', icon=ft.icons.PERSON, content=contenido_perfil),
+            ft.Tab(text='Configuración', icon=ft.icons.SETTINGS, content=contenido_config)
         ],
         expand=1
     )
